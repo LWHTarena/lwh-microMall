bac1d72b17628dcef6a00385e29f0b407b52d4ef
liwh <lwhtarena@163.com>
2019-02-16 12:29:46 +0800

    Initial commit
    分布式构建的小商城

---
+++ doc/readme.md
@@ -0,0 +1,10 @@
+# 微服务搭建的商城
+
+## 前端选型的技术框架
+
+- angular 7.x + ngrx
+
+## 后端选型的技术框架
+
+- spring cloud + OAuth2.0
+
---
+++ lwh-micromall-gateway/src/main/java/com/lwhtarena/microMall/App.java
@@ -0,0 +1,13 @@
+package com.lwhtarena.microMall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
